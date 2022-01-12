9d91163bf4ffb93c6d8efd767784538d7c9f8ba5
hironomiu <hironomiu@gmail.com>
2022-01-13 08:55:39 +0900

    commited

--- src/api/v1/users.ts
+++ src/api/v1/users.ts
@@ -1,9 +1,14 @@
-import { Router } from 'express'
+import { Request, Router } from 'express'
 
 const users = Router()
 
-users.route('/').get(async (req, res) => {
+users.route('/').get(async (req: any, res) => {
+  console.log(req.session.userId)
   res.json({ message: 'api/v1/users' })
 })
 
+users.route('/:id').get(async (req, res) => {
+  console.log(req.params.id)
+  res.json({ message: req.params.id })
+})
 export default users
