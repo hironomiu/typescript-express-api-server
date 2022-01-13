4d074810d7362767ae244a23f5a26bef0fd31940
hironomiu <hironomiu@gmail.com>
2022-01-13 14:09:32 +0900

    commited

--- __tests__/auth.spec.ts
+++ __tests__/auth.spec.ts
@@ -1,13 +1,15 @@
 import supertest from 'supertest'
 import mysql from 'mysql2'
 import { app } from '../src/app'
+import dotenv from 'dotenv'
 
 const resetUsers = async () => {
+  dotenv.config()
   const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'mysql',
-    database: 'express_api',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     port: 3306,
   })
   connection.connect()
