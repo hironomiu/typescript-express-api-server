820b3fada45fdc61bb7b75a9a90fd2c73bfef0b8
hironomiu <hironomiu@gmail.com>
2022-02-05 00:03:07 +0000

    commited

--- src/authPassport.ts
+++ src/authPassport.ts
@@ -73,7 +73,7 @@ export const checkAuthentication = (
   if (req.session.userId) {
     next()
   } else {
-    res.status(400).json({ isLogin: false })
+    res.status(400).json({ isSuccess: false, message: 'false' })
   }
 }
 export default authPassport
