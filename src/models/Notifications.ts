58fc2902f8a8f864ec129b218f3c7fff69662be1
hironomiu <hironomiu@gmail.com>
2022-02-16 08:11:43 +0000

    commited

--- src/models/Notifications.ts
+++ src/models/Notifications.ts
@@ -4,11 +4,12 @@ import promisePool from '../database'
 export type Notifications = {
   title: string
   notification: string
+  is_confirmed: boolean
 }
 
 export const findAll = async (id: number) => {
   const [rows, fields] = await promisePool.query(
-    'select a.title,a.notification from user_notifications b inner join notifications a on a.id = b.notification_id where b.user_id = ?',
+    'select a.title,a.notification,b.is_confirmed from user_notifications b inner join notifications a on a.id = b.notification_id where b.user_id = ?',
     [id]
   )
   return rows
