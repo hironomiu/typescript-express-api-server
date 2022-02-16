632a45b7b9b671cdb342d37610ad44e7a99b76a8
hironomiu <hironomiu@gmail.com>
2022-02-16 08:03:04 +0000

    commited

--- src/api/v1/notifications.ts
+++ src/api/v1/notifications.ts
@@ -12,12 +12,6 @@ notifications.route('/').get(async (req: any, res) => {
     isSuccess: true,
     message: 'api/v1/notifications',
     data: rows,
-    // [
-    //   { title: 'test1', notification: 'test1testtest' },
-    //   { title: 'test2', notification: 'test2testtest' },
-    //   { title: 'test3', notification: 'test3testtest' },
-    //   { title: 'test4', notification: 'test4testtest' },
-    // ],
   })
 })
 
